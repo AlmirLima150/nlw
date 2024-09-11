0360762714c25bebfa89830ea4cc0756c69bcbff
almirlima150 <almirlima140@gmail.com>
2024-09-11 14:18:42 -0300

    inicia o menu, funcao cadastrar e listar

---
+++ index.js
@@ -0,0 +1,73 @@
+const { select, input, checkbox } = require("@inquirer/prompts")
+const start = async () => {
+    
+    let meta ={
+        value: "Ler um capitulo por dia",
+        checked: false
+    }
+
+    let metas = [meta]
+
+    const cadastarMeta = async() => {
+        const meta = await input({ message: "Digite a meta:"})
+
+        if (meta.length == 0 ) {
+            console.log("A meta não pode ser vazia")
+            return
+        }
+        metas.push({ value: meta, checked: false })
+    }
+
+    const listarMetas = async() => {
+        const respostas = await checkbox({
+            message:"Use as setas para movimentar, espaço para marcar e entar para confirmar",
+            choices: [...metas]
+        })
+
+        if (respostas.length == 0) {
+            console.log("Nenhuma meta selecionada!")
+            return
+        }
+
+        respostas.forEach((resposta) => {
+            const meta = metas.find((m) => {
+                return m.value == resposta
+            })
+        })
+            
+        }
+
+
+    while (true){
+        const opcao =  await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+        switch (opcao){
+            case "cadastrar":
+                await cadastarMeta()
+                console.log(metas)
+                break 
+            case "listar":
+                console.log("listando")
+                break
+            case "sair":
+                return 
+        }
+    }
+}
+
+start()
